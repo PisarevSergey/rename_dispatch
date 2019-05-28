8aff937e6ccec814741a6519660567da95e37a01
sergey pisarev <pisarev@tssltd.ru>
2019-05-28 20:16:32 +0300

    fast and reissued i/o dispatch

--- rename_dispatch/set_info_dispatch.cpp
+++ rename_dispatch/set_info_dispatch.cpp
@@ -18,6 +18,16 @@ set_info_dispatch::pre(_Inout_ PFLT_CALLBACK_DATA    data,
     }
     info_message(SET_INFO_DISPATCH, "this is rename operation");
 
+    if (FLT_IS_FASTIO_OPERATION(data))
+    {
+      info_message(SET_INFO_DISPATCH, "failing fast i/o, request irp-based operation");
+      fs_stat = FLT_PREOP_DISALLOW_FASTIO;
+      break;
+    }
+    ASSERT(FLT_IS_IRP_OPERATION(data));
+    info_message(SET_INFO_DISPATCH, "this is irp-based operation, continue dispatch");
+
+
     FILE_RENAME_INFORMATION* rename_info(static_cast<FILE_RENAME_INFORMATION*>(data->Iopb->Parameters.SetFileInformation.InfoBuffer));
     rename_info = rename_info;
     data->Iopb->Parameters.SetFileInformation.ReplaceIfExists;
@@ -29,12 +39,33 @@ set_info_dispatch::pre(_Inout_ PFLT_CALLBACK_DATA    data,
 }
 
 FLT_POSTOP_CALLBACK_STATUS
-set_info_dispatch::post(_Inout_  PFLT_CALLBACK_DATA       /*data*/,
+set_info_dispatch::post(_Inout_  PFLT_CALLBACK_DATA       data,
                         _In_     PCFLT_RELATED_OBJECTS,
                         _In_opt_ PVOID                    /*completion_context*/,
-                        _In_     FLT_POST_OPERATION_FLAGS /*flags*/)
+                        _In_     FLT_POST_OPERATION_FLAGS flags)
 {
   FLT_POSTOP_CALLBACK_STATUS fs_stat(FLT_POSTOP_FINISHED_PROCESSING);
+
+  do
+  {
+    if (FLTFL_POST_OPERATION_DRAINING & flags)
+    {
+      info_message(SET_INFO_DISPATCH, "filter unloading, skip processing");
+      break;
+    }
+    info_message(SET_INFO_DISPATCH, "filter is not unloading, continue processing");
+
+    if (TRUE == FLT_IS_REISSUED_IO(data))
+    {
+      info_message(SET_INFO_DISPATCH, "reissued i/o, skipping");
+      break;
+    }
+    info_message(SET_INFO_DISPATCH, "not a reissued i/o, continue processing");
+
+    //FLT_POSTOP_MORE_PROCESSING_REQUIRED
+
+  } while (false);
+  //FLT_IS_REISSUED_IO();
 
   return fs_stat;
 }
