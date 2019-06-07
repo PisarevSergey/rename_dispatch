123996e2eb815ea0e57e65aee315af44e512ec43
sergey pisarev <pisarev@tssltd.ru>
2019-06-07 17:02:33 +0300

    removed test app

--- rename_test/file_ops.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace file_ops
-{
-  void rename_file(const wchar_t* base_dir_full_path,
-    const wchar_t* src_file_name,
-    const wchar_t* target_dir_name,
-    const wchar_t* target_file_name);
-}
